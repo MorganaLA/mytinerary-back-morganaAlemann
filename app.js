f38a1213f525e9b25df90aeaeca4ea45f0ff5da6
MorganaLA <moriluz97@gmail.com>
2024-08-13 18:14:59 -0300

    final3.1

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ import './config/database.js';
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-
 import indexRouter from './router/index.router.js';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 const corsOptions = {
-  origin: 'https://main--mytinerary-mla.netlify.app', 
+  origin: 'https://mytinerary-mla.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 };
@@ -21,8 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
 app.use('/api', indexRouter);
+
 app.head('/', (req, res) => {
   res.status(200).send('OK');
 });
 
 app.listen(PORT, () => console.log('Server running on port: ' + PORT));
+
